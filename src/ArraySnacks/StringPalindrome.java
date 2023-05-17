2448cf5c681cb07310812ef2ad8833b7eb18ce7a
ebubejohnenyi <ebubejohnenyi@gmail.com>
2023-05-17 13:31:26 +0100

    Update

--- src/ArraySnacks/StringPalindrome.java
+++ src/ArraySnacks/StringPalindrome.java
@@ -6,6 +6,23 @@ import java.util.Scanner;
 
 public class StringPalindrome {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String reversed = "";
+        System.out.print("Enter a name: ");
+        String userInput = input.nextLine();
+        boolean checker = false;
+        for (int index = userInput.length() - 1; index >= 0; index--) {
+            reversed += userInput.charAt(index);
+            if(reversed.equals(userInput)){
+                checker = true;
+            }
+        }
+        if(checker){
+            System.out.println("This is a palindrome.");
+        }
+        else System.out.println("This is not a palindrome.");
 
+        System.out.println(reversed);
     }
+
 }
