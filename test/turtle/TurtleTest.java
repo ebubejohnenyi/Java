19fefbd936fc37c4b9f4d2be8f13edbf3cc6a830
ebubejohnenyi <ebubejohnenyi@gmail.com>
2023-05-23 10:42:45 +0100

    Update

---
+++ test/turtle/TurtleTest.java
@@ -0,0 +1,17 @@
+//package turtle;
+//
+//import ChapterSeven.turtle.Turtle;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
+//
+//public class TurtleTest {
+//    Turtle sam;
+//
+//    @BeforeEach
+//    @Test
+//    public void testWeHaveATurtle(){
+//        sam = new Turtle();
+//    }
+//    @Test
+//    void
+//}
